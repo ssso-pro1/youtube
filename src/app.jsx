b82fba84f580d22c4838c855d91247b725114058
Soyeon Jang <ssso.pro1@gmail.com>
2021-08-26 16:10:51 +0900

    search_header ui css

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
 function App() {
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  // const API_KEY = process.env.REACT_APP_API_KEY;
 
   const [videos, setVideos] = useState([]);
 
@@ -24,7 +25,12 @@ function App() {
       .catch(error => console.log('error', error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
